167504e54638d63f5985f9bd1cb2b713881c7474
paaaging <ikashakai@gmail.com>
2021-11-25 12:32:52 +0900

    フロント更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,42 @@
 <template>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card>
-          <v-card-title class="justify-center font-bold primary">
+  <v-app>
+        <v-card class="elevation-0 rounded-t indigo lighten-5">
+          <v-card-text class="pb-0">
+            <p class="text-center text-h6 font-weight-black">
+              <v-icon>mdi-clipboard-check-outline</v-icon>
             文字数チェッカー
-          </v-card-title>
+            </p>
+          </v-card-text>
         </v-card>
-        <v-card class="logo py-4 d-flex justify-center">
-          <v-textarea
-            v-model="inputText"
-            name=""
-            label="入力欄"
-            value="文字数をカウントするテキストを入力して下さい。"
-            hint="こちらにテキストを入力して下さい。"
-          ></v-textarea>
+        <v-container class="mt-0">
+        <v-card class="elevation-0 mb-3 rounded indigo darken-4">
+          <v-card-text class="pb-1">
+            <p class="text-center text-h6 white--text font-weight-black">
+            テキスト入力欄
+            </p>
+          </v-card-text>
         </v-card>
-          <p class="char-box">文字数: <span>{{ textCount }}</span> </p>
-          <button class="btn-reset" @click="reset">リセット</button>
-      </v-col>
-    </v-row>
+            <v-card class="elevation-0 pb-0">
+              <v-textarea
+                v-model="inputText"
+                name="input"
+                background-color="indigo lighten-5"
+                filled
+                auto-grow
+                hint="※こちらにテキストを入力して下さい。"
+              ></v-textarea>
+            </v-card>
+            <v-row>
+              <v-col cols="auto" class="mr-auto">
+              <p class="char-box font-weight-black"><strong class="orange--text text--darken-3">文字数 : <span>{{ textCount }}</span></strong></p>
+              </v-col>
+              <v-col cols="auto">
+              <v-btn class="mr-8 elevation-0 rounded indigo darken-4 white--text font-weight-black" @click="reset">リセット</v-btn>
+              </v-col>
+            </v-row>
+              
+        </v-container>
+  </v-app>
 </template>
 
 <script>
